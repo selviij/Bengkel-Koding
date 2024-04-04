61410c5287b11ba7bba350ea53e8b8a0a936c311
selviij <112878616+selviij@users.noreply.github.com>
2024-04-04 16:13:51 +0700

    To do list

---
+++ kegiatan/koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+$databaseHost = 'localhost';
+$databaseName = 'kegiatan';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, 
+    $databasePassword, $databaseName);
